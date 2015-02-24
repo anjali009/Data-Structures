aa32bf8b1a6b279301a227ed25212f5b38f8ef1e
anjali009 <cse.anjalimenon@gmail.com>
2015-02-24 18:09:18 +0530

    Bubble Sort

---
+++ bubble_sort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[10] ;
+    int temp = 0 ;
+    cout << "\nEnter the elements into the array : " ;
+    for(int i = 0; i < 10 ; i++)
+    {
+        cin >> arr[i] ;
+    }
+    cout << "\nSorting using the bubble sort!" ;
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = i+1; j < 10; j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                temp = arr[i] ;
+                arr[i] = arr[j] ;
+                arr[j] = temp ;
+            }
+        }
+    }
+    cout << "\nTherefore, the sorted array is : " ;
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "\n" << arr[i] ;
+    }
+    return 0;
+
+}
+
